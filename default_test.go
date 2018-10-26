acaca9987970e4ab651f82447418c5dd37de3a60
flo <flo@cis.lmu.de>
2018-10-26 16:58:18 +0200

    Check time format in unit tests

--- default_test.go
+++ default_test.go
@@ -2,29 +2,35 @@ package logger
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestDefaultLogger(t *testing.T) {
+	tformat := "20060102"
 	tests := []struct {
 		want, fmt     string
 		a, b, c       interface{}
 		edebug, debug bool
 	}{
-		{" INFO 1 2 3\n", "%d %d %d", 1, 2, 3, false, false},
-		{" DEBUG 1 2 3\n", "%d %d %d", 1, 2, 3, true, true},
+		{"%s INFO 1 2 3\n", "%d %d %d", 1, 2, 3, false, false},
+		{"%s DEBUG 1 2 3\n", "%d %d %d", 1, 2, 3, true, true},
 		{"", "%d %d %d", 1, 2, 3, false, true},
 	}
 	for _, tc := range tests {
 		t.Run(tc.want, func(t *testing.T) {
 			buf := &bytes.Buffer{}
-			l := New(WithWriter(buf), WithTimeFormat(""))
+			l := New(WithWriter(buf), WithTimeFormat(tformat))
 			Set(l)
 			EnableDebug(tc.edebug)
 			if tc.debug {
 				Debug(tc.fmt, tc.a, tc.b, tc.c)
 			} else {
 				Info(tc.fmt, tc.a, tc.b, tc.c)
+			}
+			if tc.want != "" {
+				tc.want = fmt.Sprintf(tc.want, time.Now().Format(tformat))
 			}
 			if got := buf.String(); got != tc.want {
 				t.Fatalf("expected %q; got %q", tc.want, got)
